5b3d458f496ea7dcccfd7b65221a5a94ccaf3f2c
piterrus0102 <piterrus@mail.ru>
2021-12-16 15:31:52 +0300

    Initial commit

---
+++ TestLibraryInMaven/src/main/java/ru/piterrus/testlibraryinmaven/MavenPiterrusUtils.kt
@@ -0,0 +1,9 @@
+package ru.piterrus.testlibraryinmaven
+
+class MavenPiterrusUtils {
+    companion object {
+        fun add(a: Int, b: Int) : Int {
+            return a + b
+        }
+    }
+}
---
+++ app/src/main/java/ru/piterrus/testlibraryinmaven/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.piterrus.testlibraryinmaven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
