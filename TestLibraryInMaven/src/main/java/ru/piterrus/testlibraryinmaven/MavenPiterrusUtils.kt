a0b0cf3d79405052987c8bc4dc321b8ef5a526a6
piterrus0102 <piterrus@mail.ru>
2021-12-20 16:02:55 +0300

    Готовый тест проект с правильными грэдлами и тасками для мавена

--- TestLibraryInMaven/src/main/java/ru/piterrus/testlibraryinmaven/MavenPiterrusUtils.kt
+++ TestLibraryInMaven/src/main/java/ru/piterrus/testlibraryinmaven/MavenPiterrusUtils.kt
@@ -5,5 +5,10 @@ class MavenPiterrusUtils {
         fun add(a: Int, b: Int) : Int {
             return a + b
         }
+
+        fun divide(a: Int, b: Int) : Int {
+            if(b == 0) throw ArithmeticException()
+            return a / b
+        }
     }
 }
